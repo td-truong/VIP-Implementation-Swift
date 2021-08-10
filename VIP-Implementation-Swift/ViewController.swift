933620714c27e36e6da65b9dc8780580d14484fa
Duy Truong <duytt@mirabo.com.vn>
2021-08-10 10:57:26 +0700

    Initial Commit

---
+++ VIP-Implementation-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIP-Implementation-Swift
+//
+//  Created by Machintosh on 10/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
