26af40e7907f766e579d2a46940392e182dc5082
Thanh Duy Truong <thanhduyhust@gmail.com>
2021-09-08 02:13:40 +0700

    Handle API callback on main thread

--- VIP-Implementation-Swift/Common/Network/UserRepository.swift
+++ VIP-Implementation-Swift/Common/Network/UserRepository.swift
@@ -25,21 +25,23 @@ class UserRepository: UserRepositoryProtocol {
     func getUsers(completion: @escaping ([User]?, Error?) -> Void) -> URLSessionDataTask {
         let usersURL = URL(string: "users", relativeTo: Endpoints.baseURL)!
         let dataTask = session.dataTask(with: usersURL) { data, response, error in
-            guard let response = response as? HTTPURLResponse,
-                  response.statusCode == 200,
-                  error == nil,
-                  let data = data else {
-                completion(nil, error)
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            do {
-                let users = try decoder.decode([User].self, from: data)
-                completion(users, nil)
-            } catch {
-                completion(nil, error)
-            }
+            DispatchQueue.main.async {
+                guard let response = response as? HTTPURLResponse,
+                      response.statusCode == 200,
+                      error == nil,
+                      let data = data else {
+                    completion(nil, error)
+                    return
+                }
+                
+                let decoder = JSONDecoder()
+                do {
+                    let users = try decoder.decode([User].self, from: data)
+                    completion(users, nil)
+                } catch {
+                    completion(nil, error)
+                }
+            }            
         }
         dataTask.resume()
         return dataTask
